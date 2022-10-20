ec78ec60fce38e10296426de34ebc0bef77f286f
celiorscosta <celiorscosta@hotmail.com>
2022-10-20 12:31:33 -0300

    setup complete

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,8 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-
+// const { compilerOptions } = require('./tsconfig.json')
+// const { pathsToModuleNameMapper } = require('ts-jest/utils')
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -88,7 +89,7 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDid>' }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
